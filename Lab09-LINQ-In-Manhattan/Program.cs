dfb10c6d8118d52d98bcd428486aac895ea633e4
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-07-31 11:26:58 -0500

    Add project files.

---
+++ Lab09-LINQ-In-Manhattan/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+public class FeatureCollection
+{
+    public string Type { get; set; }
+    public List<Feature> Features { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Read the JSON data from the file
+        string jsonFilePath = "data.json";
+        string jsonData = File.ReadAllText(jsonFilePath);
+
+        // Deserialize the JSON data into the FeatureCollection
+        FeatureCollection featureCollection = JsonConvert.DeserializeObject<FeatureCollection>(jsonData);
+
+        // Now you can access the Features property to get the list of Feature objects
+        List<Feature> features = featureCollection.Features;
+
+        bool finished = false;
+
+        while (!finished)
+        {
+            // Question 1: Output all of the neighborhoods in this data list (Final Total: 147 neighborhoods)
+            var allNeighborhoods = features.Select(feature => feature.Properties.Neighborhood);
+            Console.WriteLine($"All Neighborhoods ({allNeighborhoods.Count()}):");
+            foreach (var neighborhood in allNeighborhoods)
+            {
+                Console.WriteLine(neighborhood);
+            }
+
+            Console.WriteLine("Press 2 to move to Question 2 or any other key to exit:");
+
+            char input = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            if (input == '2')
+            {
+                // Question 2: Filter out all the neighborhoods that do not have any names (Final Total: 143)
+                var nonEmptyNeighborhoods = features
+                    .Where(feature => !string.IsNullOrEmpty(feature.Properties.Neighborhood))
+                    .Select(feature => feature.Properties.Neighborhood);
+                Console.WriteLine($"Non-empty Neighborhoods ({nonEmptyNeighborhoods.Count()}):");
+                foreach (var neighborhood in nonEmptyNeighborhoods)
+                {
+                    Console.WriteLine(neighborhood);
+                }
+
+                Console.WriteLine("Press 3 to move to Question 3 or any other key to exit:");
+
+                input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                if (input == '3')
+                {
+                    // Question 3: Remove the duplicates (Final Total: 4 neighborhoods)
+                    var distinctNeighborhoods = features
+                        .Where(feature => !string.IsNullOrEmpty(feature.Properties.Neighborhood))
+                        .Select(feature => feature.Properties.Neighborhood)
+                        .Distinct();
+                    Console.WriteLine($"Distinct Neighborhoods ({distinctNeighborhoods.Count()}):");
+                    foreach (var neighborhood in distinctNeighborhoods)
+                    {
+                        Console.WriteLine(neighborhood);
+                    }
+
+                    Console.WriteLine("Press 4 to move to Question 4 or any other key to exit:");
+
+                    input = Console.ReadKey().KeyChar;
+                    Console.WriteLine();
+
+                    if (input == '4')
+                    {
+                        // Question 4: Rewrite the queries from above and consolidate all into one single query.
+                        var result = features
+                            .Where(feature => !string.IsNullOrEmpty(feature.Properties.Neighborhood))
+                            .Select(feature => feature.Properties.Neighborhood)
+                            .Distinct();
+                        Console.WriteLine($"Consolidated Query Result ({result.Count()}):");
+                        foreach (var neighborhood in result)
+                        {
+                            Console.WriteLine(neighborhood);
+                        }
+
+                        Console.WriteLine("Press 5 to move to Question 5 or any other key to exit:");
+
+                        input = Console.ReadKey().KeyChar;
+                        Console.WriteLine();
+
+                        if (input == '5')
+                        {
+                            // Question 5: Rewrite at least one of these questions only using the opposing method.
+                            // Using LINQ Query statements instead of LINQ method calls
+                            var queryResult = (from feature in features
+                                               where !string.IsNullOrEmpty(feature.Properties.Neighborhood)
+                                               select feature.Properties.Neighborhood).Distinct();
+                            Console.WriteLine($"Using LINQ Query ({queryResult.Count()}):");
+                            foreach (var neighborhood in queryResult)
+                            {
+                                Console.WriteLine(neighborhood);
+                            }
+
+                            finished = true; // Exits the loop and terminates the program after Question 5
+                        }
+                        else
+                        {
+                            finished = true;
+                        }
+                    }
+                    else
+                    {
+                        finished = true;
+                    }
+                }
+                else
+                {
+                    finished = true;
+                }
+            }
+            else
+            {
+                finished = true;
+            }
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# Lab 09: LINQ in Manhattan
+Author: Mollemira Porphura
+Version: 1.0.0
+
+## Overview
+This project is a C# program that utilizes LINQ queries and Lambda expressions to read data from an external JSON file and perform various data filtering and manipulation operations. The program specifically focuses on location information for properties in Manhattan.
+
+## Getting Started
+To run the program, follow these steps:
+
+1. Clone or download this repository to your local machine.
+2. Make sure you have the required dependencies installed, including Newtonsoft.Json (NewtonSoftJson).
+3. Compile and run the C# program using your preferred IDE or command-line tools.
+
+## Example
+
+1. Launch the program, it will automatically output all of the neighborhoods in the data list.
+2. Press 2 to filter out neighborhoods that do not have any names.
+3. Prees 3 to remove duplicate neighborhoods.
+4. Press 4 to consolidate the queries into a single query.
+5. Press 5 to do the same thing but using the the opposing method..
+
+The results of each task will be displayed in the console.
+
+## Architecture
+The project is written in C# and utilizes LINQ queries and Lambda expressions to process the data. The `Newtonsoft.Json` (NewtonSoftJson) NuGet package is used for reading and deserializing the data from the `data.json` file.
+
+## Change Log
+- Version 1.0.0: Initial release with all specified functionalities and questions answered. (Date: DD/MM/YYYY)
+
+Please note that this README serves as a general template and should be updated with accurate and relevant information specific to your project. Additionally, you can include sections like Installation, Usage, Contribution Guidelines, License, etc., if applicable to your project.
